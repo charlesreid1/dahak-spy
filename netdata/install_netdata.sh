fcc2975fa988864854e95e1efe1fec172c035011
Chaz Reid <charlesreid1@Gmail.com>
2018-03-09 15:52:56 -0800

    make it work for headless/automated installs

--- netdata/install_netdata.sh
+++ netdata/install_netdata.sh
@@ -3,4 +3,4 @@
 # Install netdata on spy,
 # so you can monitor while you monitor
 
-bash <(curl -Ss https://my-netdata.io/kickstart.sh)
+bash <(curl -Ss https://my-netdata.io/kickstart.sh) --dont-wait
